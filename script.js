67890323a10dc14cd51908d80ecf42818e3172fa
MichelSJS <michelsjs.12@gmail.com>
2021-08-05 18:01:33 -0300

    adição de movimento a personagem

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let direction = "right";
 
 function criarBackground(){
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 16*box, 16*box);
 }
 
 function criarCobra(){
@@ -19,6 +19,15 @@ function criarCobra(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up"); direction = "down";
 }
 
 function iniciarJogo(){
@@ -30,8 +39,8 @@ function iniciarJogo(){
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
