2b6e42c809bd03c8a4134e5cb916674bce3652a8
MichelSJS <michelsjs.12@gmail.com>
2021-08-09 16:51:00 -0300

    Game Over e estilização

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ let food = {
 }
 
 function criarBackground(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16*box, 16*box);
 }
 
 function criarCobra(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "blue";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function criarComida(){
-    context.fillStyle = "red";
+    context.fillStyle = "purple";
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -40,10 +40,18 @@ function update(event){
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBackground();
     criarCobra();
